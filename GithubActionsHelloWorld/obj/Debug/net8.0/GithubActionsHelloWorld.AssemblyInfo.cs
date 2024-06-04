31accba613a1aa57e3a2e8ff55a882d6438dae17
baoh13 <dzacu1a@gmail.com>
2024-06-04 12:02:12 +0100

    Adding git ignore

--- GithubActionsHelloWorld/obj/Debug/net8.0/GithubActionsHelloWorld.AssemblyInfo.cs
+++ GithubActionsHelloWorld/obj/Debug/net8.0/GithubActionsHelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GithubActionsHelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dce452520ab287b3703575f6d602b66368c0b2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2572e7357f12d825b28b0e571998d9ede2df29b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GithubActionsHelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GithubActionsHelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
